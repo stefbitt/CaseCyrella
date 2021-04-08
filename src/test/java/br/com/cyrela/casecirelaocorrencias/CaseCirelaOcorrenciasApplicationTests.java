092477d52e9b44f768b05796b4e349454590228f
Marcio Antunes <mantunes001@gmail.com>
2021-04-08 12:18:57 -0300

    Cyrela Ocorrencias inicializado

---
+++ src/test/java/br/com/cyrela/casecirelaocorrencias/CaseCirelaOcorrenciasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cyrela.casecirelaocorrencias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseCirelaOcorrenciasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
